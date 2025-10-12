87f6a335e32096ce962ce1688d69d794ccd42674
Kiryl Baranouski <kirlanby@gmail.com>
2025-10-12 12:48:28 +0000

    feat: add basic CORS configuration

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,8 +1,23 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+origins = [
+    "http://localhost",
+    "http://localhost:8080",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_origin_regex=r"^https?://(localhost|127\.0\.0\.1|\[::1\])(:\d+)?$",
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 @app.get("/")
-async def root():
+async def main():
     return {"message": "Hello World"}
