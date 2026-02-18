66fdf43795230e453524db39a87a82c2bf283996
Kiryl Baranouski <kirlanby@gmail.com>
2026-02-18 14:03:54 +0100

    [fix] celery containers work

--- apps/core-backend/app/tasks/monitoring_tasks.py
+++ apps/core-backend/app/tasks/monitoring_tasks.py
@@ -1,47 +1,37 @@
 # app/tasks/monitoring_tasks.py
-import asyncio
-import ipaddress
-
-from ..app.services.device_services import get_all_devices
-from ..app.services.ping_services import insert_ping_result
+from app.services import ping_services
 from celery import shared_task
 from celery.exceptions import MaxRetriesExceededError, TimeoutError
-from icmplib import async_ping as ping
+from icmplib import ping
 from sqlalchemy.exc import SQLAlchemyError
+
+from ..models import PingResult, device
+
+# --- Importy modeli i sesji DB
+
 
 PING_COUNT = 5
 PING_TIMEOUT = 2.0
 
 
 @shared_task(bind=True, name="device_icmp")
-async def device_icmp(self, device_id: int, device_address: str):
-    address = device_address
+def device_icmp(self, device_address: str):
+
     result = {"status": "ERROR", "reason": "Unknown error."}
 
     try:
-        ipaddress.ip_address(address)
-    except ValueError:
 
-        return {
-            "status": "ERROR",
-            "reason": f"Bad IP format: {device_address}. Test requires a valid IP address.",
-        }
+        address = device_address
 
-    try:
+        # 2. Wykonanie Pingu (ICMP)
+        host = ping(address, count=PING_COUNT, timeout=PING_TIMEOUT, privileged=False)
 
-        host = await ping(
-            address, count=PING_COUNT, timeout=PING_TIMEOUT, privileged=False
-        )
-
-        await insert_ping_result(
-            device_id=device_id,
+        # 3. Zapis Wyników (do TimescaleDB) -- tu podobno funkcje robi kacper????
+        ping_services.insert_ping_result(
             ip_address=host.address,
             is_alive=host.is_alive,
             rtt_avg_ms=host.avg_rtt,
             packet_loss_percent=host.packet_loss,
-            diagnostic_message=(
-                "host available" if host.is_alive else "host unreachable"
-            ),
         )
         return (
             {
@@ -55,11 +45,9 @@ async def device_icmp(self, device_id: int, device_address: str):
         )
 
     except SQLAlchemyError as db_err:
-        result = {"status": "DB_ERROR", "reason": f"Database error: {str(db_err)}"}
-        return result
+        result = {"status": "ERROR", "reason": f"Database error: {str(db_err)}"}
     except Exception as e:
         result = {"status": "ERROR", "reason": f"General error: {str(e)}"}
-        return result
 
 
 @shared_task(name="schedule_all_pings")
@@ -67,17 +55,15 @@ def schedule_all_pings(only_active: bool = True):
 
     count = 0
     try:
-        devices = asyncio.run(get_all_devices())
-
-        if not devices:
-            return "No devices found."
-
+        devices = (
+            ping_services.get_all_devices()
+        )  # Pobranie wszystkich urządzeń z bazy danych kiryl mówi że jakoś ma byc ale jakoś nie ma sensu
         if only_active:
             devices = [d for d in devices if d.is_active]
 
         for device in devices:
             try:
-                device_icmp.delay(device_address=device.ip_address, device_id=device.id)
+                device_icmp.delay(device_address=device.ip_address)
                 count += 1
             except (MaxRetriesExceededError, TimeoutError) as e:
                 print(f"Failed to schedule ping for {device.ip_address}: {str(e)}")
