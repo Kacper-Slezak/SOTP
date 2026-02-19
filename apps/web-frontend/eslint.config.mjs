3fbbd6b93ccdf84d4db8de1b05c6adcb0a168ffb
Kacper <k.slezak2207@gmail.com>
2026-02-19 19:42:45 +0100

    fix: Security check

---
+++ apps/web-frontend/eslint.config.mjs
@@ -0,0 +1,43 @@
+import { defineConfig } from "eslint/config";
+import nextCoreWebVitals from "eslint-config-next/core-web-vitals";
+import globals from "globals";
+import tsParser from "@typescript-eslint/parser";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import js from "@eslint/js";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+    baseDirectory: __dirname,
+    recommendedConfig: js.configs.recommended,
+    allConfig: js.configs.all
+});
+
+export default defineConfig([{
+    extends: [
+        ...compat.extends("eslint:recommended"),
+        ...compat.extends("plugin:@typescript-eslint/recommended"),
+        ...nextCoreWebVitals
+    ],
+
+    languageOptions: {
+        globals: {
+            ...globals.browser,
+            ...globals.node,
+        },
+
+        parser: tsParser,
+        ecmaVersion: 2021,
+        sourceType: "module",
+
+        parserOptions: {
+            ecmaFeatures: {
+                jsx: true,
+            },
+        },
+    },
+
+    rules: {},
+}]);
